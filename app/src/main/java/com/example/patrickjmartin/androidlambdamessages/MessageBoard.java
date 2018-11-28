257563015889436d58f64102692e8f943ee04d30
Patrick Martin <patrickmartin88@gmail.com>
2018-11-28 14:40:08 -0800

    Completed Messages POJO. Implementing the MessageBoard POJO.

---
+++ app/src/main/java/com/example/patrickjmartin/androidlambdamessages/MessageBoard.java
@@ -0,0 +1,9 @@
+package com.example.patrickjmartin.androidlambdamessages;
+
+import java.util.ArrayList;
+
+public class MessageBoard {
+
+    private String title, identifier;
+    private ArrayList<Message> messages;
+}
