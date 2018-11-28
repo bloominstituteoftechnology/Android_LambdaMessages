37e7a927985b5cea907dee18a260cbdede3a5615
Patrick Martin <patrickmartin88@gmail.com>
2018-11-28 15:07:21 -0800

    Choppy implementation of MessageBoard. I'm unclear on directions.

--- app/src/main/java/com/example/patrickjmartin/androidlambdamessages/MessageBoard.java
+++ app/src/main/java/com/example/patrickjmartin/androidlambdamessages/MessageBoard.java
@@ -1,9 +1,48 @@
 package com.example.patrickjmartin.androidlambdamessages;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class MessageBoard {
+public class MessageBoard implements Parcelable {
 
     private String title, identifier;
     private ArrayList<Message> messages;
+
+    public MessageBoard(JSONObject jsonObject, String identifier) {
+        this.identifier = identifier;
+
+        try {
+            this.title = jsonObject.getString("title");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(identifier);
+    }
 }
