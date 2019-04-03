86c46a5d38f1e7519d6b9e31b022508d86c712c4
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-03 15:19:37 -0400

    Initial commit

---
+++ MessageBoard/app/src/main/java/com/rybarstudios/messageboard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rybarstudios.messageboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
