d2b537ac3ba5a4e4a3d4f5db193d8c21aab79279
Shoonsk8 <shoon@att.net>
2019-04-10 15:23:33 -0400

    Create a POJO class called Message with the following data members

---
+++ app/src/main/java/com/lambda/android_lambdamessages/Message.java
@@ -0,0 +1,6 @@
+package com.lambda.android_lambdamessages;
+
+public class Message {//Create a POJO class called Message with the following data members
+    String sender, text, id;
+    double timestamp;
+}
