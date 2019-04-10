5c250962cf1ecc10c225f61a046bf22441d5498e
Shoonsk8 <shoon@att.net>
2019-04-10 15:39:01 -0400

    Create constructors which accept all members, sender and text, and a JSONObject

--- app/src/main/java/com/lambda/android_lambdamessages/Message.java
+++ app/src/main/java/com/lambda/android_lambdamessages/Message.java
@@ -1,6 +1,13 @@
 package com.lambda.android_lambdamessages;
 
-public class Message {//Create a POJO class called Message with the following data members
+public class Message {//1 Create a POJO class called Message with the following data members
     String sender, text, id;
     double timestamp;
+//2 Create constructors which accept all members, sender and text, and a JSONObject
+    public Message(String sender, String text, String id, double timestamp) {//The key's for the JSON object match the names of the variables
+        this.sender = sender;
+        this.text = text;
+        this.id = id; //The default value for id will be null
+        this.timestamp = timestamp; //The default value for timestamp will be System.currentTimeMillis() / 1000 which gives us time since the Unix Epoch (January 1970) in seconds
+    }
 }
